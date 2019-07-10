fa7b50e3af476c1ee7cadac51b8f03352f5c7603
PrinceEddy <mukoeddy8@gmail.com>
2019-07-10 20:34:40 +0800

    Updates

---
+++ precache-manifest.a8e16a32857c0c085fb0acc009b47d31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ca51ca486beafae9bc9efbcdf609a0",
+    "url": "/myMonsters/index.html"
+  },
+  {
+    "revision": "6cc58e14554e80648ed0",
+    "url": "/myMonsters/static/css/main.ce07f1a9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/myMonsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6cc58e14554e80648ed0",
+    "url": "/myMonsters/static/js/main.3330a2a0.chunk.js"
+  },
+  {
+    "revision": "89c1f0ea28eabdd06919",
+    "url": "/myMonsters/static/js/runtime~main.0d88cd58.js"
+  }
+]);
